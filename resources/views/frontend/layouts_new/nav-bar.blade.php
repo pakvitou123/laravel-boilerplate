7297a9bd8f511c87e1687a481331ddb29553a653
pakvitou <pakvitou168@gmail.com>
2017-08-29 15:17:54 +0700

    create register

--- resources/views/frontend/layouts_new/nav-bar.blade.php
+++ resources/views/frontend/layouts_new/nav-bar.blade.php
@@ -6,7 +6,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="">Stack overflow</a>
+            <a class="navbar-brand" href="{{route('frontend.index')}}">Stack overflow</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <div class="col-md-6 hidden-xs">
