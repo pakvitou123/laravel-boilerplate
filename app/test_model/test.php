8d2187da8d98171010901ddf4537e5cf46f48a6a
pakvitou <pakvitou168@gmail.com>
2017-09-09 11:36:27 +0700

    design interface and answer question

---
+++ app/test_model/test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\model1;
+
+use Illuminate\Database\Eloquent\Model;
+
+class test extends Model
+{
+    //
+}
---
+++ resources/views/frontend/layouts_new/home_page/left-side.blade.php
@@ -0,0 +1,19 @@
+
+<div class="container" id="question-group" >
+        <div class="right-side-create">
+            <span>Create</span>
+        </div>
+        <a href="#" class="btn-creategroup">
+            <img src="{{asset('../img/group-icon/Group-icon.png')}}" style="width: 50px;height: 50px;border-radius: 50%;border:1px solid #ffffff;"><span id="new-group">New Group</span><span id="new-group-content">Creat your own group</span>
+        </a>
+            <hr style="margin-bottom:10px;">
+    <div class="right-side-question">
+        <span>Add a question</span>
+        <a href="{{route('question')}}">
+        <div class="col-md3">
+            <img src="{{asset('../img/group-icon/sign-question-icon.png')}}" style="width: 50px;height: 50px;border-radius: 50%;border:1px solid #ffffff;">
+            <span id="new-question-content">Ask people about question and what to share. </span>
+        </div>
+        <button class="btn-create-question"> Add question</button> </a>
+    </div>
+</div>
