2bbb864c3f319603ca3c8dee9b45d9a9408a885b
pakvitou <pakvitou168@gmail.com>
2017-09-26 11:02:52 +0700

    background on click any questions.

--- resources/views/frontend/layouts_new/nav-bar.blade.php
+++ resources/views/frontend/layouts_new/nav-bar.blade.php
@@ -49,8 +49,9 @@
                         <ul id="nav">
                             <li id="notification_li">
                                 <span id="notification_count">3</span>
-                                <a href="#" id="notificationLink"><i class="fa fa-globe fa-2x notifications-menu "
-                                                                     aria-hidden="true"></i></a>
+                                <div id="notificationLink">
+                                    <i class="fa fa-globe fa-2x notifications-menu "
+                                       aria-hidden="true"></i></div>
                                 <div id="notificationContainer">
                                     <div id="notificationTitle" style="border: none">Notifications</div>
                                     <div id="notificationsBody" class="notifications">
@@ -59,14 +60,12 @@
                                                 <button type="button" class="list-group-item"
                                                         style="margin-left: -15px;width:348px;border-radius: 0px">
                                                     <div class="col-md-3">
-
                                                         <img src="{{asset('/img/profile/smilelaugh.jpg')}}"
                                                              alt="Todd Shelton" class="img-responsive img-circle"
                                                              style="width: 40px;margin-left: -20px">
 
                                                     </div>
                                                     <div class="col-md-10" style="margin-left: -48px;">
-                                                    <span>
                                                         <span class="fwb">Savorn Bon</span>
                                                         <span class="ask"> asked to join </span>
                                                         <span class="">Test&nbsp;ITC</span>
@@ -75,12 +74,12 @@
                                                                    src="https://static.fpnh1-1.fna.fbcdn.net/rsrc.php/v3/yX/r/eJy9hr6FcSf.png"
                                                                    alt=""></span>
                                                         <span style="color: #90949c;font-size: 12px;">• 2 hours ago </span>
-                                                    </span>
+                                                        </span>
                                                     </div>
                                                 </button>
                                             </div>
                                         </div>
-                                        <div class="list-group">
+                                        <div class="list-group" style="cursor: pointer;" onclick="window.location='http://google.com';">
                                             <div class="col-md-12">
                                                 <button type="button" class="list-group-item"
                                                         style="margin-left: -15px;width:348px;border-radius: 0px">
@@ -92,22 +91,24 @@
 
                                                     </div>
                                                     <div class="col-md-10" style="margin-left: -48px;">
-                                                    <span>
-                                                        <span class="fwb">Savorn Bon</span>
-                                                        <span class="ask"> asked to join </span>
-                                                        <span class="">Test&nbsp;ITC</span>
-                                                        <span>.</span>
-                                                        <span><img class="_10cu img _8o _8r img"
-                                                                   src="https://static.fpnh1-1.fna.fbcdn.net/rsrc.php/v3/yX/r/eJy9hr6FcSf.png"
-                                                                   alt=""></span>
-                                                        <span style="color: #90949c;font-size: 12px;">• 2 hours ago </span>
-                                                    </span>
+                                                        <span>
+                                                            <span class="fwb">Savorn Bon</span>
+                                                            <span class="ask"> asked to join </span>
+                                                            <span class="">Test&nbsp;ITC</span>
+                                                            <span>.</span>
+                                                            <span><img class="_10cu img _8o _8r img"
+                                                                       src="https://static.fpnh1-1.fna.fbcdn.net/rsrc.php/v3/yX/r/eJy9hr6FcSf.png"
+                                                                       alt=""></span>
+                                                            <span style="color: #90949c;font-size: 12px;">• 2 hours ago </span>
+                                                        </span>
                                                     </div>
                                                 </button>
                                             </div>
                                         </div>
                                     </div>
-                                    <div id="notificationFooter"><a href="#">See All</a></div>
+                                    <a href="#" style="cursor: pointer;" onclick="window.location='http://google.com';">
+                                        <div id="notificationFooter">See All</div>
+                                    </a>
                                 </div>
                             </li>
                         </ul>
@@ -118,5 +119,4 @@
     </div>
 </nav>
 <div class="container">
-
 </div>
